31f05b4a1c7cd2176fab6c6435a319fb54fd59ce
tiktoksixseven676767-del <tiktoksixseven676767@gmail.com>
2026-05-09 03:08:18 +0200

    Create money.js

---
+++ plugins/money.js
@@ -0,0 +1,36 @@
+let handler = async (m, { conn, usedPrefix, command }) => {
+    // Definizione della cifra enorme
+    let amount = 10000e390 
+    
+    // Accediamo al database dell'utente
+    let user = global.db.data.users[m.sender]
+    
+    // Aggiungiamo i soldi alla variabile .euro
+    user.euro += amount
+    
+    // Reazione per confermare l'azione
+    await conn.sendMessage(m.chat, {
+        react: {
+            text: '💰',
+            key: m.key
+        }
+    })
+
+    m.reply(`
+╭━━⊱「 『 💰 』 \`MONEY\` 」
+│ 
+│ ➸ *Hai ricevuto:* │ ➸ ∞ Euro 🪙
+│ 
+│ 『 👝 』 \`Nuovo Saldo:\` 
+│ ➸ *Infinito*
+*╰⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*`)
+}
+
+handler.help = ['money']
+handler.tags = ['xp']
+handler.command = ['money'] 
+
+// Se vuoi che solo il proprietario possa usarlo, decommenta la riga sotto:
+handler.rowner = true 
+
+export default handler
